638f93a6b1a4282e8bd8834116bec0921d180c4e
JBZonda <jeroen.vanderborgh@gmail.com>
2023-04-05 14:25:26 +0200

    #17 change password met functies

--- change_password.php
+++ change_password.php
@@ -1,25 +1,10 @@
 <?php
 function show_content($data) {
-    echo '<div class="register-login">
-    <form class="form_register" method="post" action="\educom-webshop-database/index.php">
-
-    <span class="error">'; echo get_variable($data,"errors","generic"); echo '</span><br>
-    <label for="name">Oud wachtwoord:</label><br>
-    <input type="password" id="name" name="old_password" >
-    <span class="error">'; echo get_variable($data,"errors", "old_password"); echo '</span><br>
-
-
-    <label for="name">Nieuw wachtwoord:</label><br>
-    <input type="password" id="name" name="password" >
-    <span class="error">'; echo get_variable($data,"errors", "password"); echo '</span><br>
-    <label for="name">Herhaal wachtwoord:</label><br>
-    <input type="password" id="name" name="password_re" ><br>
-
-    <input type="hidden" name="page" value="change_password"><br>
-    <input type="submit" value="Submit">
-    </form>
-    </div>
-    ';
+    show_form_start("register-login","form_register", $data);
+    show_form_field("old_password", "Oud wachtwoord:", "password", $data, "old_password");
+    show_form_field("password", "Nieuw wachtwoord:", "password", $data, "password");
+    show_form_field("password", "Herhaal wachtwoord:", "password", $data, "password");
+    show_form_end("Submit", "change_password");
 }
 
 ?>
